b7de760bc37499a8132070b1e1e07933e4f79f10
Apoorva Chaubal <chaubal.4@osu.edu>
2023-07-04 03:30:18 -0500

    removed unused imports

--- networks/DQN.py
+++ networks/DQN.py
@@ -1,14 +1,5 @@
-import torch
 import torch.nn    as nn
-import torch.optim as optim
 import torch.nn.functional as F
-
-import math
-import random
-import matplotlib
-import matplotlib.pyplot as plt
-from collections import namedtuple, deque
-from itertools import count
 
 class DQN(nn.Module):
     def __init__(self,num_obs,num_act):
