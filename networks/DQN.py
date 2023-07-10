7dc5db6088e20ae0813af3fb01030dee49bd7adf
Apoorva Chaubal <chaubal.4@osu.edu>
2023-07-10 00:25:28 -0500

    added helpful syntax to describe function i/o datatype

--- networks/DQN.py
+++ networks/DQN.py
@@ -1,14 +1,15 @@
-import torch.nn    as nn
+import torch as T
+import torch.nn as nn
 import torch.nn.functional as F
 
 class DQN(nn.Module):
-    def __init__(self, obsDims:list ,actDims:list):
+    def __init__(self, obsDims:list ,actDims:list) -> None:
         super(DQN, self).__init__()
         self.layer1 = nn.Linear(*obsDims, 128)
         self.layer2 = nn.Linear(128, 128)
         self.layer3 = nn.Linear(128, *actDims)
 
-    def forward(self, observation):
+    def forward(self, observation:T.Tensor) -> T.Tensor:
         action = F.relu(self.layer1(observation))
         action = F.relu(self.layer2(action))
         action = self.layer3(action)
