ffaed8a37ae9cdddd550c73fecce06dce2b7b483
Apoorva Chaubal <chaubal.4@osu.edu>
2023-07-21 22:10:59 -0500

    fixed sample method

--- agent/Memory.py
+++ agent/Memory.py
@@ -11,11 +11,11 @@ class Memory:
     def __len__(self) -> int: #TODO: delete if unused
         return len(self.memory)
     
-    def push(self, data) -> None:
+    def push(self, data) -> None: #TODO: can I not specify datatype?
         self.index              = self.index % self.capacity
         self.memory[self.index] = data
         self.index             += 1
    
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
+    def sample(self, batch_size:int):
+        return np.random.choice(self.memory, batch_size)
     
